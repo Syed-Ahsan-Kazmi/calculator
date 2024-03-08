40c36c0f9ca40de65f1dde4d5a9f759c6982ce15
Syed Ahsan Kazmi <ahsan1kazmi2@gmail.com>
2024-03-08 22:48:56 +0500

    add new index.js file

---
+++ index.js
@@ -0,0 +1,27 @@
+let cal    = parseInt(prompt("Enter Your Number +",""));
+let cal1   = parseInt(prompt("Enter Your Number +",""));
+let result = cal + cal1;
+alert(result);
+
+let a  = parseInt(prompt("Enter Your Number -",""));
+let b  = parseInt(prompt("Enter Your Number -",""));
+let c  = a - b;
+alert(c);
+
+let d  = parseInt(prompt("Enter Your Number *",""));
+let e  = parseInt(prompt("Enter Your Number *",""));
+let f  = d * e;
+alert(f);
+
+let g  = parseInt(prompt("Enter Your Number /",""));
+let h  = parseInt(prompt("Enter Your Number /",""));
+let i  = g / h;
+alert(i);
+
+
+
+
+
+
+
+
